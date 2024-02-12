f41141f1391e85982f35da7f8c6691471190016d
Apostolos Giannoulidis <32461460+agiannoul@users.noreply.github.com>
2024-02-12 15:07:39 +0200

    Update example2.py

--- example2.py
+++ example2.py
@@ -4,3 +4,4 @@ import numpy as np
 
 streamModule =PdmModule()
 prediction = streamModule.collect_data(pd.to_datetime("01/01/2023 15:00:15"), np.array([0.52, 0.32,0.22,0.16,0.15,0.12,0.13]), id="1")
+print(prediction.toString())
